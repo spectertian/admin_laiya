c78a3ce63db65c477fa446887a7295c619657243
zhongsheng <tianzhongsheng168@gmail.com>
2022-02-07 11:54:29 +0800

    sonata

---
+++ src/Admin/ListAdmin.php
@@ -0,0 +1,33 @@
+<?php
+// src/Admin/CategoryAdmin.php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+final class ListAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form->add('name', TextType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagrid): void
+    {
+        $datagrid->add('name');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list->addIdentifier('name');
+    }
+
+//    protected function configureShowFields(ShowMapper $show): void
+//    {
+//        $show->add('name');
+//    }
+}
