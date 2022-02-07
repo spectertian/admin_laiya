289ac7967297562fbd92a6e83e2817bb4e8939a0
zhongsheng <tianzhongsheng168@gmail.com>
2022-02-07 11:12:47 +0800

    sonata

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineMongoDBAdminBundle\SonataDoctrineMongoDBAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
